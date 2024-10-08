a38223744c29e384151ec32bd58921aa560733fd
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-10-08 17:29:45 +0300

    Created the sign up and sign in pages and added  route protection

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Image from 'next/image'
+import {ClerkLoaded, ClerkLoading, SignUp} from "@clerk/nextjs";
+import {Loader2} from "lucide-react";
+import Logo from '@/images/logo.svg'
+function Page() {
+    return(
+
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 ">
+            <div className="h-full lg:flex flex-col items-center justify-center px-4">
+
+                <div className="text-center space-y-4 pt-16">
+
+                    <h1 className="font-bold text-3xl text-">
+                        Welcome Back !
+                    </h1>
+
+                    <p className="text-base text-">
+                        Log In or Create an account to get back to your dashboard
+                    </p>
+
+                </div>
+                <div className="flex items-center justify-center">
+                    <ClerkLoaded>
+                        <SignUp path='/sign-up'/>
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground"/>
+                    </ClerkLoading>
+
+                </div>
+
+            </div>
+
+            <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+                <Image src={Logo} height={100} width={100} alt="logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Page;
